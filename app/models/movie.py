0af68e9e54c5e893d0d12b4459fcf82c5449f0cd
KitLuc <tobongoomez809@gmail.com>
2023-07-16 22:35:06 -0500

    feat(database): add Movie model

--- app/models/movie.py
+++ app/models/movie.py
@@ -4,11 +4,11 @@ from config.database import BASE
 
 class Movie(BASE):
     __tablename__ = 'TBL_Movies'
-    id: int = Column(Integer, primary_key = True, autoincrement=1)
+    id: int = Column(Integer, primary_key = True)
     title: str = Column(String, nullable = False)
     year: int = Column(Integer, nullable = False)
     override: str = Column(String, nullable = False)
+    director: str = Column(String, nullable = False)
     national: str = Column(String, nullable = False)
     gender: str = Column(String, nullable=False)
     budget: float = Column(Float, nullable = False)
-    director: str = Column(String, nullable = False)

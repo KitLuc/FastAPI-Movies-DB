3003d6bf507ab63f007d5775e75193423f5cc56e
KitLuc <tobongoomez809@gmail.com>
2023-07-17 22:18:56 -0500

    feat(routes): update users routes

---
+++ app/routes/user.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from models.user import User as UserModel
+from config.database import SESSION
+from schemas.user import User
+
+
+app = APIRouter()
+
+
+@app.get("/users/", status_code=200, response_model=list[User])
+async def get_all_users() -> list[User]:
+    DB = SESSION()
+    users = await DB.query(UserModel).all()
+    return users
+
+
+@app.get("/users/{user_id}", status_code=200, response_model=User)
+async def get_user_by_id(user_id: int) -> User:
+    DB = SESSION()
+    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
+    return user
+
+
+@app.post("/users/", status_code=201)
+async def create_user(user: User):
+    DB = SESSION()
+    new_user = await UserModel(**user.dict())
+    DB.add(new_user)
+    DB.commit()
+    return {"message": "User created"}
+
+
+@app.put("/users/{user_id}", status_code=200)
+async def update_user(user_id: int, user: User):
+    DB = SESSION()
+    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
+
+    for key, value in user.dict().items():
+        setattr(user, key, value)
+
+    return {"message": "User updated"}
+
+
+@app.delete("/users/{user_id}", status_code=200)
+async def delete_user(user_id: int):
+    DB = SESSION()
+    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
+    DB.delete(user)
+    DB.commit()
+    return {"message": "User deleted"}

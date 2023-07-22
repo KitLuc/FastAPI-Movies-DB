486b404795aa4a57392af5171ed7fcd841d1997b
KitLuc <tobongoomez809@gmail.com>
2023-07-21 22:06:07 -0500

    feat(database): change attribute email in User class model

--- app/models/user.py
+++ app/models/user.py
@@ -6,6 +6,6 @@ class User(BASE):
     __tablename__ = 'TBL_Users'
     id:Integer = Column(Integer, primary_key = True)
     name:String = Column(String, nullable = False)
-    user:String = Column(String, nullable = False)
+    email:String = Column(String, nullable = False)
     password:String = Column(String, nullable = False)
     

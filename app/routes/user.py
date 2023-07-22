83d849562d969d3c32904f8885e243d551e1228c
KitLuc <tobongoomez809@gmail.com>
2023-07-21 22:08:21 -0500

    feat(routes): add login in route, and delete lines inecesary

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,51 +1,51 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-from models.user import User as UserModel
+from fastapi.responses import JSONResponse
 from config.database import SESSION
 from schemas.user import User
+from services.user import UserService
+from utils.jwt_manager import create_token
 
 
 user = APIRouter()
 
 
+@user.post("/login/", tags=['auth'], status_code=200, response_model=list[User])
+def login(user: User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token: str = create_token(user.dict())
+        return JSONResponse(status_code=200, content={"token": token})
+
+
 @user.get("/users/", status_code=200, response_model=list[User])
-async def get_all_users() -> list[User]:
+def get_all_users() -> list[User]:
     DB = SESSION()
-    users = await DB.query(UserModel).all()
+    users = UserService(DB).get_users()
     return users
 
 
 @user.get("/users/{user_id}", status_code=200, response_model=User)
-async def get_user_by_id(user_id: int) -> User:
+def get_user_by_id(user_id: int) -> User:
     DB = SESSION()
-    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
+    user = UserService(DB).get_user_by_id(user_id)
     return user
 
 
 @user.post("/users/", status_code=201)
-async def create_user(user: User):
+def create_user(user: User):
     DB = SESSION()
-    new_user = await UserModel(**user.dict())
-    DB.add(new_user)
-    DB.commit()
+    UserService(DB).create_user(user)
     return {"message": "User created"}
 
 
-@user.put("/users/{user_id}", status_code=200)
-async def update_user(user_id: int, user: User):
+@user.put("/users/{id}", status_code=200)
+def update_user(id: int, user: User):
     DB = SESSION()
-    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
-
-    for key, value in user.dict().items():
-        setattr(user, key, value)
-
+    UserService(DB).update_user(id, user)
     return {"message": "User updated"}
 
 
-@user.delete("/users/{user_id}", status_code=200)
-async def delete_user(user_id: int):
+@user.delete("/users/{id}", status_code=200)
+def delete_user(id: int):
     DB = SESSION()
-    user = await DB.query(UserModel).filter(UserModel.id == user_id).first()
-    DB.delete(user)
-    DB.commit()
+    UserService(DB).delete_user(id)
     return {"message": "User deleted"}
